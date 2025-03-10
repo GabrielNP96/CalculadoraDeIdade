19a9fee7d3f41e9bf4a944c49e33f535f97c543c
Gabriel Nascimento <123754383+GabrielNP96@users.noreply.github.com>
2025-03-10 01:09:20 +0000

    salvando dados

--- CalculadoraDeIdade/src/components/Input/index.tsx
+++ CalculadoraDeIdade/src/components/Input/index.tsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
+
 const Input = () => {
+    const [day,setDay] = useState("");
+    const [mounth, setMounth] = useState("");
+    const [year, setYear] = useState("");
     return (
         <form className="calculator" action="#">
             <input type="number" placeholder="Dia"/>
             <input type="number" placeholder="Mês"/>
             <input type="number" placeholder="Ano" />
-            <button>Calcular</button>
+            <button onClick={}>Calcular</button>
         </form>
     )
 }
